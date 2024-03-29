d4d86a0fdd42255edcda37af89c36a0aa48eb93f
acycoco <128036362+acycoco@users.noreply.github.com>
2024-03-29 22:05:18 +0900

    [Silver IV] Title: 수 찾기, Time: 1472 ms, Memory: 54684 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    static int[] num;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        num = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        //정렬을 먼저해야됨
+        Arrays.sort(num);
+        int m = Integer.parseInt(br.readLine());
+        int[] target = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            target[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //찾아야 될 애 for문
+        for (int i = 0; i < m; i++) {
+            boolean result = binarySearch(target[i], 0, n - 1);
+            if (result) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+
+    private static boolean binarySearch(int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target == num[mid]) {
+                return true;
+            } else if (target < num[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
+
+
