2ae0e14f654236d534a0dfb3dfd9bb45393fc9d3
acycoco <128036362+acycoco@users.noreply.github.com>
2024-05-14 17:20:01 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18104 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                int result1 = dp[i / 2] + 1;
+                if (dp[i] > result1) {
+                    dp[i] = result1;
+                }
+            }
+            if (i % 3 == 0) {
+                int result1 = dp[i / 3] + 1;
+                if (dp[i] > result1) {
+                    dp[i] = result1;
+                }
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+    
+}
