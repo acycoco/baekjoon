c44cd8361b51adc6c90bd105b7d5c13ae851068c
acycoco <128036362+acycoco@users.noreply.github.com>
2024-06-20 00:09:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 24216 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,6 +1,8 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -13,47 +15,46 @@ public class Main {
         int subin = Integer.parseInt(st.nextToken());
         int syster = Integer.parseInt(st.nextToken());
 
-        int max = 0;
-        if (syster > subin) {
-            max = syster;
-        } else {
-            max = subin;
-        }
-        boolean[] visited = new boolean[100001];
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{subin, 0});
+        int[] dist = new int[200001];
+        Arrays.fill(dist, -1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(subin);
+        dist[subin] = 0;
 
         if (subin == syster) {
             System.out.println(0);
             return;
         }
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            visited[now[0]] = true;
 
-            int next1 = now[0] - 1;
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            int next1 = now - 1;
             if (next1 == syster) {
-                System.out.println(now[1] + 1);
+                System.out.println(dist[now] + 1);
                 break;
             }
-            if (next1 >= 0 && !visited[next1]) {
-                queue.add(new int[]{next1, now[1] + 1});
+            if (next1 >= 0 && dist[next1] == -1) {
+                dist[next1] = dist[now] + 1;
+                queue.add(next1);
             }
-            int next2 = now[0] + 1;
+            int next2 = now + 1;
             if (next2 == syster) {
-                System.out.println(now[1] + 1);
+                System.out.println(dist[now] + 1);
                 break;
             }
-            if (next2 <= 100000 && !visited[next2]) {
-                queue.add(new int[]{next2, now[1] + 1});
+            if (next2 <= 200000 && dist[next2] == -1) {
+                dist[next2] = dist[now] + 1;
+                queue.add(next2);
             }
-            int next3 = now[0] * 2;
-            if (next3 == syster ) {
-                System.out.println(now[1] + 1);
+            int next3 = now * 2;
+            if (next3 == syster) {
+                System.out.println(dist[now] + 1);
                 break;
             }
-            if (next3 <= 100000 && !visited[next3]) {
-                queue.add(new int[]{next3, now[1] + 1});
+            if (next3 <= 200000 && dist[next3] == -1) {
+                dist[next3] = dist[now] + 1;
+                queue.add(next3);
             }
         }
     }
