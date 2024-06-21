de85dca5f1c33d406b0e1c225f2f088f47acec07
acycoco <128036362+acycoco@users.noreply.github.com>
2024-06-21 13:44:12 +0900

    [Silver III] Title: N과 M (1), Time: 420 ms, Memory: 33904 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<int[]> result = new ArrayList<>();
+    static int n;
+    static int m;
+    static boolean[] used;
+    static int[] now;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        now = new int[m];
+        used = new boolean[n + 1];
+        fill(0);
+        bw.close();
+    }
+
+    static void fill(int next) throws IOException {
+        if (next == m) {
+            for (int i = 0; i < m; i++) {
+                bw.append(now[i] + " ");
+            }
+            bw.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                now[next] = i;
+                fill(next + 1);
+                used[i] = false;
+            }
+        }
+    }
+}
