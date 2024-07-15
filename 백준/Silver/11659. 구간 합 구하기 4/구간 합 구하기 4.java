20fe3784f3830bb434f245213b51d4c33d47399b
acycoco <128036362+acycoco@users.noreply.github.com>
2024-07-15 17:59:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1124 ms, Memory: 58732 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,30 +1,27 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //입력받기
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        //숫자 입력받기
-        int[] num = new int[n];
-        int[] sum = new int[n];
-        for (int i = 0; i < n; i++) {
-            num[i] = sc.nextInt();
-            //합배열만들기
-            if (i == 0) sum[i] = num[i];
-            else sum[i] = sum[i-1] + num[i];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int test = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        int[] sum = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum[i] = arr[i] + sum[i - 1];
         }
-        //for m만큼 구간합구하기
-        for (int i = 0; i < m; i++) {
-            //i랑 j 에 -1 해준 범위의 구간합구하기 s[j] - s[i-1]
-            int start = sc.nextInt() - 1;
-            int end = sc.nextInt() - 1;
-            int minus;
-            if (start == 0) minus = 0;
-            else minus = sum[start - 1];
-            System.out.println(sum[end] - minus);
+
+        for (int i = 0; i < test; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            System.out.println(sum[to] - sum[from - 1]);
         }
     }
 }
