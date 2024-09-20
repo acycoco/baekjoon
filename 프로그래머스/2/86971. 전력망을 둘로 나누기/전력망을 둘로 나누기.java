a488ebd08d5ec4a04318e528a12b308db18373a8
acycoco <128036362+acycoco@users.noreply.github.com>
2024-09-20 19:24:03 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.47 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,51 +1,94 @@
 import java.util.*;
 class Solution {
-    public static class Edge {
-        int start;
-        int end;
-        public Edge(int start, int end) {
-            this.start = start;
-            this.end = end;
+
+    public int solution(int n, int[][] wires) {
+        int minDiff = n;
+        for(int i = 0; i < wires.length; i++) {
+            int[] parent = new int[n + 1]; //연결되어 있는 작은 노드를 부모로
+            int[] rank = new int[n + 1];
+            for(int j = 1; j <= n; j++) {
+                parent[j] = j;
+            }
+            for(int j = 0; j < wires.length; j++) {
+                if(i == j) 
+                    continue;
+                union(wires[j][0], wires[j][1], parent);
+            }
+            int diff = Math.abs(n - countNodes(1, n, parent) * 2);
+            minDiff = Math.min(diff, minDiff);
         }
-        public boolean isSame(int[] wire) {
-            return (wire[0] == this.start && wire[1] == this.end) ||
-                (wire[0] == this.end && wire[1] == this.start);
+        return minDiff;
+    }
+    
+    public int find(int a, int[] parent) {
+        if(a == parent[a]) 
+            return a;
+        return parent[a] = find(parent[a], parent);
+    }
+    
+    public void union(int a, int b, int[] parent) {
+        int parentA = find(a, parent);
+        int parentB = find(b, parent);
+        
+        if(parentA == parentB) {
+            return;       
+        } else if (parentA < parentB) {
+            parent[parentA] = parentB;
+        } else {
+            parent[parentB] = parentA;
         }
         
     }
-    public int solution(int n, int[][] wires) {
-        List<Edge> edges = new ArrayList<>();
-        for(int[] wire: wires) {
-            edges.add(new Edge(wire[0], wire[1]));
-            edges.add(new Edge(wire[1], wire[0]));
-        }
-  
-        int min = n;
-        for(int i = 0; i < n - 1; i++) {
-            min = Math.min(min,  countAbsDifference(edges, new boolean[n + 1], wires[i], n));
-        }
-        return min;
-    }
     
-    public int countAbsDifference(List<Edge> edges, boolean[] visited, int[] deletedWire, int n) {
-        return Math.abs(n - visitGraph(1, edges, visited, deletedWire) * 2);
-    }
-    public int visitGraph(int start, List<Edge> edges, boolean[] visited, int[] deletedWire) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        visited[start] = true;
-        int count = 1;
-        while(!queue.isEmpty()) {
-            int current = queue.poll();
-            for(Edge edge: edges) {
-                if(edge.start == current && !visited[edge.end] && !edge.isSame(deletedWire)) {
-                    queue.offer(edge.end);
-                    visited[edge.end] = true;
-                    count++;
-                }
+    public int countNodes(int a, int n, int[] parent) {
+        int count = 0;
+        int parentA = find(a, parent);
+        for(int i = 1; i <= n; i++) {
+            if(find(i, parent) == parentA) {
+                count++;
             }
         }
-       
         return count;
     }
+//     public int solution(int n, int[][] wires) {
+//         List<List<Integer>> edges = new ArrayList<>();
+//         for(int i = 0; i <= n; i++) {
+//             edges.add(new ArrayList<>());
+//         }
+        
+//         for(int[] wire: wires) {
+//             edges.get(wire[0]).add(wire[1]);
+//              edges.get(wire[1]).add(wire[0]);
+//         }
+  
+//         int min = n;
+//         for(int i = 0; i < n - 1; i++) {
+//             min = Math.min(min,  countAbsDifference(edges, new boolean[n + 1], wires[i], n));
+//         }
+//         return min;
+//     }
+    
+//     public int countAbsDifference(List<List<Integer>> edges, boolean[] visited, int[] deletedWire, int n) {
+//         return Math.abs(n - visitGraph(1, edges, visited, deletedWire) * 2);
+//     }
+    
+//     public int visitGraph(int start, List<List<Integer>> edges, boolean[] visited, int[] deletedWire) {
+//         Queue<Integer> queue = new LinkedList<>();
+//         queue.offer(start);
+//         visited[start] = true;
+//         int count = 1;
+//         while(!queue.isEmpty()) {
+//             int current = queue.poll();
+//             for(int neighbor: edges.get(current)) {
+//                 if(!visited[neighbor] && !(deletedWire[0] == current && deletedWire[1] == neighbor)
+//                   && !(deletedWire[1] == current && deletedWire[0] == neighbor)) {
+//                     queue.offer(neighbor);
+//                     visited[neighbor] = true;
+//                     count++;
+//                 }
+//             }
+//         }
+       
+//         return count;
+//     }
 }
