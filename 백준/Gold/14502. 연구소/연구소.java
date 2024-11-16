095d7d95a52965132e5688364fa0b060f5582290
acycoco <128036362+acycoco@users.noreply.github.com>
2024-11-16 22:46:18 +0900

    [Gold IV] Title: 연구소, Time: 284 ms, Memory: 64656 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int n;
@@ -12,7 +10,16 @@ public class Main {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int maxSafe;
+    static List<Point> emptySpace = new ArrayList<>();
 
+    public static class Point {
+        int x;
+        int y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,59 +32,72 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        buildWall(0);
+        calcEmptySpace();
+        buildWall(0, 0, new int[3]);
         System.out.println(maxSafe);
     }
 
-    public static void buildWall(int count) {
-        if(count == 3) {
-            boolean[][] visited = new boolean[n][m];
-            for (int i = 0; i < n; i++) {
-                for(int j = 0; j < m; j++) {
-                    if(arr[i][j] == 2) {
-                        bfs(i, j, visited);
-                    }
+    public static void calcEmptySpace() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    emptySpace.add(new Point(i, j));
                 }
             }
-            maxSafe = Math.max(maxSafe, countSafe(visited));
+        }
+    }
+
+    public static void buildWall(int depth, int start, int[] selected) {
+        if(depth == 3) {
+            simulate(selected);
             return;
         }
 
+        for(int i = start; i < emptySpace.size(); i++) {
+            selected[depth] = i;
+            buildWall(depth + 1,i + 1, selected);
+        }
+    }
+
+    public static void simulate(int[] selected) {
+        int[][] temp = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
-                    arr[i][j] = 1;
-                    buildWall(count + 1);
-                    arr[i][j] = 0;
+            System.arraycopy(arr[i], 0, temp[i], 0, m);
+        }
+
+        for (int i = 0; i < selected.length; i++) {
+            Point seletedPoint = emptySpace.get(selected[i]);
+            temp[seletedPoint.x][seletedPoint.y] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (temp[i][j] == 2) {
+                    dfs(new Point(i, j), temp);
                 }
+            }
+        }
+
+        maxSafe = Math.max(maxSafe, countSafe(temp));
+    }
+    public static void dfs(Point point, int[][] temp) {
+        for (int i = 0; i < 4; i++) {
+            int nextX = point.x + dx[i];
+            int nextY = point.y + dy[i];
+            if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < m &&
+                    temp[nextX][nextY] == 0
+            ) {
+                temp[nextX][nextY] = 2;
+                dfs(new Point(nextX, nextY), temp);
             }
         }
     }
 
-    public static void bfs(int x, int y, boolean[][] visited) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        visited[x][y] = true;
-        while(!queue.isEmpty()) {
-            int[] now = queue.poll();
-            for(int i = 0; i < 4; i++) {
-                int nextX = now[0] + dx[i];
-                int nextY = now[1] + dy[i];
-                if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < m &&
-                        !visited[nextX][nextY] && arr[nextX][nextY] == 0
-                ) {
-                    queue.add(new int[]{nextX, nextY});
-                    visited[nextX][nextY] = true;
-                }
-            }
-        }
-    }
-
-    public static int countSafe(boolean[][] visited) {
+    public static int countSafe(int[][] temp) {
         int count = 0;
         for (int i = 0; i< n; i++) {
             for (int j = 0; j < m; j++) {
-                if(!visited[i][j] && arr[i][j] == 0) {
+                if(temp[i][j] == 0) {
                     count++;
                 }
             }
