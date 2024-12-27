7b7db7b53854913fb106dc5e46654383d60feed3
acycoco <128036362+acycoco@users.noreply.github.com>
2024-12-28 01:32:18 +0900

    [Gold IV] Title: 테트로미노, Time: 396 ms, Memory: 53968 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -2,6 +2,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,76 +11,73 @@ public class Main {
     static int n;
     static int m;
     static int[][] arr;
+    static boolean[][] visited;
     static int max = 0;
-    static int[][][] blocks = {
-            {{0, 1}, {0, 2}, {0, 3}},  // ㅡ
-            {{1, 0}, {2, 0}, {3, 0}},  // ㅣ
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int maxNumber;
 
-            {{0, 1}, {1, 0}, {1, 1}},  // ㅁ
-
-            {{1, 0}, {2, 0}, {2, 1}},  // ㄴ
-            {{0, 1}, {0, 2}, {1, 0}},
-            {{0, 1}, {1, 1}, {2, 1}},
-            {{0, 1}, {0, 2}, {-1, 2}},
-            {{0, 1}, {-1, 1}, {-2, 1}},  // ㄱ
-            {{1, 0}, {1, 1}, {1, 2}},
-            {{0, 1}, {1, 0}, {2, 0}},
-            {{0, 1}, {0, 2}, {1, 2}},
-
-            {{1, 0}, {1, 1}, {2, 1}},  // ㄹ
-            {{0, 1}, {-1, 1}, {-1, 2}},
-            {{1, 0}, {1, -1}, {2, -1}},
-            {{0, 1}, {1, 1}, {1, 2}},
-
-            {{0, 1}, {0, 2}, {1, 1}},  // ㅜ
-            {{1, 0}, {2, 0}, {1, 1}},  // ㅏ
-            {{0, 1}, {0, 2}, {-1, 1}}, // ㅗ
-            {{1, 0}, {2, 0}, {1, -1}}  // ㅓ
-    };
-
-    //    static int[][][] blocks = {
-//            {{0, 1}, {0, 2}, {0, 3}}, {{1, 0}, {2, 0}, {3, 0}}, //1
-//            {{0, 1}, {1, 0}, {1, 1}}, //2
-//            {{1, 0}, {2, 0}, {2, 1}}, {{1, 0}, {2, 0}, {2, -1}}, {{-1, 0}, {1, 0}, {2, 0}}, {{1, 0}, {2, 0}, {0, 1}}, //3대칭
-//            {{0, 1}, {0, 2}, {1, 0}}, {{0, 1}, {1, 1}, {2, 1}}, {{1, 0}, {1, -1}, {1, -2}}, //3회전
-//            {{1, 0}, {1, 1}, {2, 1}}, {{0, 1}, {1, 0}, {1, -1}}, {{0, 1}, {1, 1}, {1, 2}}, {{0, 1}, {-1, 1}, {1, 0}}, //4 대칭, 회전
-//            {{0, 1}, {1, 1}, {0, 2}}, {{0, 1}, {-1, 1}, {1, 1}}, {{1, -1}, {1, 0}, {1, 1}}, {{1, 0}, {2, 0}, {1, 1}} //5회전
-//    };
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[n][m];
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        
+        maxNumber = getMax(arr);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                for(int[][] block : blocks) {
-                    max = Math.max(max, calc(block, i, j));
-                }
+                visited[i][j] = true;
+                List<int[]> selected = new ArrayList<>();
+                selected.add(new int[]{i, j});
+                dfs(1, selected, arr[i][j]);
+                visited[i][j] = false;
             }
         }
         System.out.println(max);
     }
+    public static void dfs(int depth, List<int[]> selected, int sum) {
+        if (sum + (4 - depth) * maxNumber <= max) {
+            return;
+        }
+        if (depth == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
 
-    public static int calc(int[][] block, int x, int y) {
-        int sum = arr[x][y];
-        for (int[] point : block) {
-            int i = point[0] + x;
-            int j = point[1] + y;
-            if (i >= 0 && i < n && j >= 0 && j < m) {
-                sum += arr[i][j];
-            }
-            else {
-                return -1;
+        for (int i = 0; i < selected.size(); i++) {
+            for (int j = 0; j < 4; j++) {
+                int[] block = selected.get(i);
+                int x = dx[j] + block[0];
+                int y = dy[j] + block[1];
+                if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y]) {
+                    continue;
+                }
+                selected.add(new int[]{x, y});
+                visited[x][y] = true;
+                dfs(depth + 1, selected, sum + arr[x][y]);
+                selected.remove(selected.size() - 1);
+                visited[x][y] = false;
             }
         }
-        return sum;
+
+    }
+    
+    public static int getMax(int[][] arr) {
+        int max = arr[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+        return max;
     }
 }
