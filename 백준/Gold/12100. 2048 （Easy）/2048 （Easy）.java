8a35e8c5dace445a277deade7d1407ed30ee63bd
acycoco <128036362+acycoco@users.noreply.github.com>
2025-01-03 21:00:52 +0900

    [Gold I] Title: 2048 (Easy), Time: 216 ms, Memory: 35220 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,7 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -9,12 +9,14 @@ import java.util.StringTokenizer;
 public class Main {
     static int n;
     static int[][] arr;
-    static int answer = -1;
-
+    static int answer;
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
+
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,83 +24,92 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         dfs(0, arr);
         System.out.println(answer);
     }
 
-    public static void dfs(int depth, int[][] board) {
+    static void dfs(int depth, int[][] arr) {
         if (depth == 5) {
-            answer = Math.max(answer, getMaxNumber(board));
+            answer = Math.max(answer, getMax(arr));
             return;
         }
 
-        for (int dir = 0; dir < 4; dir++) {
-            int[][] newBoard = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                System.arraycopy(board[i], 0, newBoard[i], 0, n);
+        //4방향으로
+        for (int i = 0; i < 4; i++) {
+            int[][] newArr = new int[n][n];
+            for (int j = 0; j < n; j++) {
+                int[] line = getLine(arr, i, j);
+                playLine(line);
+                writeLine(newArr, line, i, j);
             }
-            for (int index = 0; index < n; index++) {
-                int[] line = getLine(newBoard, dir, index);
-                mergeLine(line);
-                copyLine(line, newBoard, dir, index);
-            }
-            dfs(depth + 1, newBoard);
+            //한 줄씩
+            //바뀐 한줄로 저장
+
+            dfs(depth + 1, newArr);
         }
     }
 
-
-    public static void copyLine(int[] line, int[][] board, int dir, int index) {
+    static void writeLine(int[][] board, int[] line, int dir, int index) {
         for (int i = 0; i < n; i++) {
-            if (dir == 0) board[index][i] = line[i];//상
-            else if (dir == 1) board[index][n - i - 1] = line[i];//하
-            else if (dir == 2) board[i][index] = line[i];//좌
-            else board[n - i - 1][index] = line[i];
+            if (dir == 0) {
+                board[index][i] = line[i];
+            } else if (dir == 1) {
+                board[index][n - i - 1] = line[i];
+            } else if (dir == 2) {
+                board[i][index] = line[i];
+            } else {
+                board[n - i - 1][index] = line[i];
+            }
         }
     }
 
-    public static void mergeLine(int[] line) {
+    static void playLine(int[] line) {
+        //line에 있는 수를 queue에 저장하고
+        //빼면서 다음 꺼랑 같으면 *2로 저장
+        //다음꺼랑 같지 않으면 그대로 저장
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             if (line[i] != 0) {
-                queue.offer(line[i]);
+                queue.add(line[i]);
             }
         }
-
+        Arrays.fill(line, 0);
         int index = 0;
-        while (!queue.isEmpty()) {
-            int target = queue.poll();
-            if (!queue.isEmpty() && queue.peek() == target) {
-                line[index++] = target * 2;
-                queue.poll();
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            if (queue.isEmpty() || queue.peek() != cur) {
+                line[index++] = cur;
             } else {
-                line[index++] = target;
+                queue.poll();
+                line[index++] = cur * 2;
             }
         }
-        for (int i = index; i < n; i++) {
-            line[i] = 0;
-        }
-    }
 
-    public static int[] getLine(int[][] board, int dir, int index) {
+    }
+    static int[] getLine(int[][] board, int dir, int index) {
         int[] newLine = new int[n];
         for (int i = 0; i < n; i++) {
-            if (dir == 0) newLine[i] = board[index][i];//상
-            else if (dir == 1) newLine[i] = board[index][n - i - 1];//하
-            else if (dir == 2) newLine[i] = board[i][index];//하
-            else newLine[i] = board[n - i - 1][index];
+            if (dir == 0) {
+                newLine[i] = board[index][i];
+            } else if (dir == 1) {
+                newLine[i] = board[index][n - i - 1];
+            } else if (dir == 2) {
+                newLine[i] = board[i][index];
+            } else {
+                newLine[i] = board[n - i - 1][index];
+            }
         }
         return newLine;
     }
 
-    public static int getMaxNumber(int[][] board) {
-        int max = 0;
+    static int getMax(int[][] arr) {
+        int max = arr[0][0];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                max = Math.max(board[i][j], max);
+                max= Math.max(max, arr[i][j]);
             }
         }
         return max;
     }
-
-
 }
