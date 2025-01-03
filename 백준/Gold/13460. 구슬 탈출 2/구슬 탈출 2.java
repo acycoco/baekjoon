50e24c5c94b07a7a448b0f8b241d045417fe6aea
acycoco <128036362+acycoco@users.noreply.github.com>
2025-01-03 20:17:37 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -8,114 +8,117 @@ import java.util.StringTokenizer;
 public class Main {
     static int n;
     static int m;
-    static final char RED = 'R';
-    static final char BLUE = 'B';
-    static final char WALL = '#';
-    static final char HOLE = 'O';
-    static int[] dx = new int[]{-1, 1, 0, 0};
-    static int[] dy = new int[]{0, 0, -1, 1};
     static char[][] arr;
-    static boolean[][][][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new char[n][m];
-        visited = new boolean[n][m][n][m];
-        int rX = 0;
-        int rY = 0;
-        int bX = 0;
-        int bY = 0;
+        int rx = 0, ry = 0;
+        int bx = 0, by = 0;
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             arr[i] = line.toCharArray();
-
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == RED) {
-                    rX = i;
-                    rY = j;
-                    arr[i][j] = '.';
-                }
-                if (arr[i][j] == BLUE) {
-                    bX = i;
-                    bY = j;
-                    arr[i][j] = '.';
+                if (arr[i][j] == 'R') {
+                    rx = i;
+                    ry = j;
+                } else if (arr[i][j] == 'B') {
+                    bx = i;
+                    by = j;
                 }
             }
         }
 
-        System.out.println(bfs(rX, rY, bX, bY));
-
+        System.out.println(bfs(rx, ry, bx, by));
     }
 
 
-    public static int bfs(int rX, int rY, int bX, int bY) {
+    static int bfs(int rx, int ry, int bx, int by) {
         Queue<State> queue = new LinkedList<>();
-        queue.offer(new State(rX, rY, bX, bY, 0));
-        visited[rX][rY][bX][bY] = true;
-
+        queue.add(new State(bx, by, rx, ry, 0));
+        boolean[][][][] visited = new boolean[n][m][n][m];
+        visited[rx][ry][bx][by] = true;
         while (!queue.isEmpty()) {
-            State current = queue.poll();
-            if (current.moves >= 10) return -1;
+            State now = queue.poll();
+            //10번 넘으면 실패
+            if (now.count >= 10) {
+                return -1;
+            }
             for (int i = 0; i < 4; i++) {
-                int[] redResult = move(current.redX, current.redY, i);
-                int[] blueResult = move(current.blueX, current.blueY, i);
+                //움직인 거리가 0이면 다른 방향으로
+                int redMove = getDistance(now.rx, now.ry, i);
+                int blueMove = getDistance(now.bx, now.by, i);
+                if (redMove == 0 && blueMove == 0) {
+                    continue;
+                }
 
-                int newRedX = redResult[0], newRedY = redResult[1];
-                int newBlueX = blueResult[0], newBlueY = blueResult[1];
+                int nrx = now.rx + dx[i] * redMove;
+                int nry = now.ry + dy[i] * redMove;
+                int nbx = now.bx + dx[i] * blueMove;
+                int nby = now.by + dy[i] * blueMove;
 
-                boolean redInHole = redResult[2] == 1;
-                boolean blueInHole = blueResult[2] == 1;
+                //빨간 구슬이 구멍을 지나가면 성공
+                //파란 구슬도 구멍에 빠지면 실패
 
-                if (blueInHole) continue;
-                if (redInHole) return current.moves + 1;
-
-                if (newRedX == newBlueX && newRedY == newBlueY) {
-                    if (redResult[3] > blueResult[3]) {
-                        newRedX -= dx[i];
-                        newRedY -= dy[i];
+                if (arr[nbx][nby] == 'O') {
+                    continue;
+                }
+                if (arr[nrx][nry] == 'O') {
+                    return now.count + 1;
+                }
+                if (nrx == nbx && nry == nby) {
+                    if (redMove > blueMove) {
+                        nrx -= dx[i];
+                        nry -= dy[i];
                     } else {
-                        newBlueX -= dx[i];
-                        newBlueY -= dy[i];
+                        nbx -= dx[i];
+                        nby -= dy[i];
                     }
                 }
-
-                if (!visited[newRedX][newRedY][newBlueX][newBlueY]) {
-                    visited[newRedX][newRedY][newBlueX][newBlueY] = true;
-                    queue.add(new State(newRedX, newRedY, newBlueX, newBlueY, current.moves + 1));
-                }
+                if (visited[nrx][nry][nbx][nby])
+                    continue;
+                queue.add(new State(nbx, nby, nrx, nry, now.count + 1));
+                visited[nrx][nry][nbx][nby] = true;
             }
         }
-
         return -1;
     }
 
-    public static int[] move(int x, int y, int dir) {
-        int[] result = new int[4];
-        
-        int distance = 0;
-        while (true) {
-            if (arr[x][y] == HOLE) break; // 구멍에 도달하면 즉시 중단
-            if (arr[x + dx[dir]][y + dy[dir]] == WALL) break; // 벽에 막히면 중단
-            x += dx[dir];
-            y += dy[dir];
-            distance++;
+    //구슬을 중력으로 움직이는 함수   몇개를 움직였는지 반환
+    static int getDistance(int x, int y, int d) {
+        int count = 0;
+        while(arr[x + dx[d]][y + dy[d]] != '#') {
+            x += dx[d];
+            y += dy[d];
+            count++;
+            if (arr[x][y] == 'O') {
+                return count;
+            }
         }
-        return new int[]{x, y, arr[x][y] == HOLE ? 1 : 0, distance};
+        return count;
     }
+    //최소 몇번 만에 니까 bfs가 맞지 않을까 싶음 State에는 빨간 거, 파란 거의 위치를 보관
+
+
+
 
     static class State {
-        int redX, redY, blueX, blueY, moves;
-
-        State(int redX, int redY, int blueX, int blueY, int moves) {
-            this.redX = redX;
-            this.redY = redY;
-            this.blueX = blueX;
-            this.blueY = blueY;
-            this.moves = moves;
+        int bx;
+        int by;
+        int rx;
+        int ry;
+        int count;
+        public State(int bx, int by, int rx, int ry, int count) {
+            this.bx = bx;
+            this.by = by;
+            this.rx = rx;
+            this.ry = ry;
+            this.count = count;
         }
     }
-
-
 }
