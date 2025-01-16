6367701a542b0dfa38667dbd8b10ff25b9c9bc05
acycoco <128036362+acycoco@users.noreply.github.com>
2025-01-16 17:25:35 +0900

    [Gold V] Title: 토마토, Time: 620 ms, Memory: 128044 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int m;
+    static int n;
+    static int h;
+    static int[][][] arr;
+    static boolean[][][] visited;
+    static Queue<int[]> queue = new LinkedList<>();
+    static int[] dx = {0, 1, 0, -1, 0, 0};
+    static int[] dy = {1, 0, -1, 0, 0, 0};
+    static int[] dh = {0, 0, 0, 0, 1, -1};
+    public static void main(String[] args) throws IOException {
+        //하루마다 위, 아래, 상하좌우 6방향 토마토 익는다.
+        //최소 일수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        visited = new boolean[h][n][m];
+        arr = new int[h][n][m];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < m; k++) {
+                    int tomato = Integer.parseInt(st.nextToken());
+                    if (tomato == 1) {
+                        queue.add(new int[]{i, j, k, 0});
+                        visited[i][j][k] = true;
+                    }
+                    arr[i][j][k] = tomato;
+                }
+            }
+        }
+
+        int answer = bfs();
+        if (isFinish()) {
+            System.out.println(answer);
+            return;
+        }
+        System.out.println(-1);
+    }
+
+    public static int bfs() {
+        int day = 0;
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int i = 0; i < 6; i++) {
+                int nx = dx[i] + cur[1];
+                int ny = dy[i] + cur[2];
+                int nh = dh[i] + cur[0];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || nh < 0 || nh >= h) {
+                    continue;
+                }
+                if (visited[nh][nx][ny] || arr[nh][nx][ny] != 0) { //이미 익었거나 안익은 토마토가 아니면
+                    continue;
+                }
+                arr[nh][nx][ny] = 1;
+                queue.add(new int[]{nh, nx, ny, cur[3] + 1});
+                visited[nh][nx][ny] = true;
+                day = Math.max(cur[3] + 1, day);
+            }
+        }
+
+        return day;
+    }
+
+    public static boolean isFinish() {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    if (arr[i][j][k] == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
