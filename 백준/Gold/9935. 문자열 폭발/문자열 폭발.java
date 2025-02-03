56496a397c49fa38b73e6bca052322c2f4da4f67
acycoco <128036362+acycoco@users.noreply.github.com>
2025-02-03 17:42:51 +0900

    [Gold IV] Title: 문자열 폭발, Time: 392 ms, Memory: 84832 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -4,48 +4,33 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    static char[] bomb;
     public static void main(String[] args) throws IOException {
         // 모든 폭발 문자열이 폭발
         // 폭발 문자열이 없을때까지
         //
 
 
-        Stack<Character> stack = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] input = br.readLine().toCharArray();
-        bomb = br.readLine().toCharArray();
+        String bomb = br.readLine();
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < input.length; i++) {
-            stack.push(input[i]);
-
-            if (stack.size() >= bomb.length) {
-                boolean matched = true;
-                for (int j = 0; j < bomb.length; j++) {
-                    if (stack.get(stack.size() - bomb.length + j) != bomb[j]) {
-                        matched = false;
-                        break;
-                    }
-                }
-
-                if (matched) {
-                    for (int j = 0; j < bomb.length; j++) {
-                        stack.pop();
-                    }
-                }
+            sb.append(input[i]);
+           
+            if (sb.length() >= bomb.length() && 
+                sb.substring(sb.length() - bomb.length()).equals(bomb)
+            ) {
+                sb.delete(sb.length() - bomb.length(), sb.length());
             }
-
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char ch :  stack) {
-            stringBuilder.append(ch);
-        }
+       
 
-        if (stringBuilder.length() == 0) {
+        if (sb.length() == 0) {
             System.out.println("FRULA");
         } else {
-            System.out.println(stringBuilder);
+            System.out.println(sb);
         }
     }
 }
