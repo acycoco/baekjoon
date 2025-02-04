e32d1b5061879fb9200f33819a5d737c1313d97d
acycoco <128036362+acycoco@users.noreply.github.com>
2025-02-04 13:15:08 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 192 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -8,6 +8,7 @@ public class Main {
     static int[] s;
     static int[] w;
     static int answer;
+    static int count;
     public static void main(String[] args) throws IOException {
         //계란의 내구도가 상대 계란의 무게만큼 깎인다.
         //0이하가 되는 순간 계란은 깨진다.
@@ -29,33 +30,26 @@ public class Main {
 
     public static void comb(int cur) {
         if (cur == n) {
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                if (s[i] <= 0) count++;
-            }
             answer = Math.max(answer, count);
             return;
         }
 
-        if (s[cur] <= 0) {
+        if (s[cur] <= 0|| count == n - 1) {
             comb(cur + 1);
             return;
         }
 
-        boolean isCrashed = false;
         for (int i = 0; i < n; i++) {
             if (i == cur || s[i] <= 0) continue;
             s[i] -= w[cur];
             s[cur] -= w[i];
-            isCrashed = true;
-            
+            if (s[i] <= 0) count++;
+            if (s[cur] <= 0) count++;
             comb(cur + 1);
+            if (s[i] <= 0) count--;
+            if (s[cur] <= 0) count--;
             s[i] += w[cur];
             s[cur] += w[i];
-        }
-
-        if (!isCrashed) {
-            comb(cur + 1);
         }
     }
 }
