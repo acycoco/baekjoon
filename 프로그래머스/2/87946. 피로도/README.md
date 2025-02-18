6495a205dd7fe634d1bc9390e638e928bfa37447
acycoco <128036362+acycoco@users.noreply.github.com>
2025-02-18 21:27:26 +0900

    [level 2] Title: 피로도, Time: 4.75 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 4.29 ms
+메모리: 74.9 MB, 시간: 4.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 06일 00:21:27
+2025년 02월 18일 21:27:24
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,55 @@
 import java.util.*;
 class Solution {
-    public int maxCount = 0;
+    boolean[] used;
+    int count;
     public int solution(int k, int[][] dungeons) {
-        visit(new boolean[dungeons.length], dungeons, k, 0);
-        // visitDungeons(dungeons, new boolean[dungeons.length], k, 0);
-        return maxCount;
+        //최소 필요 피로도가 있어야 탐험
+        //소모 피로도는 탐험 후 차감
+        //하루에 한번씩 탐험 던전
+        //최대한 많은 던전
+        used = new boolean[dungeons.length];
+        
+        comb(k, 0, 0, dungeons);
+        return count;
+    }
+    public void comb(int curK, int visitedCount, int depth, int[][] dungeons) {
+        
+        if(depth == dungeons.length) {
+            count = Math.max(count, visitedCount);
+            return;
+        }
+        
+        boolean isGG = false;
+        for(int i = 0; i < dungeons.length; i++) {
+            if(used[i] || curK - dungeons[i][0] < 0) continue;
+            isGG = true;
+            used[i] = true;
+            comb(curK - dungeons[i][1], visitedCount + 1, depth + 1, dungeons);
+            used[i] = false;
+        }
+        if(!isGG) {
+            count = Math.max(count, visitedCount);
+            return;
+        }
     }
     
-    public void visit(boolean[] visited, int[][] dungeons, int curK, int count) {
-        maxCount = Math.max(count, maxCount);
-       for(int i = 0; i < dungeons.length; i++) {
-           if(!visited[i] && curK >= dungeons[i][0]) {
-               visited[i] = true;
-               visit(visited, dungeons, curK - dungeons[i][1], count + 1);
-               visited[i] = false;
-           }
-       }
-    }
+    
+    
+    
+    
+    
+    
+    
+    // public void visit(boolean[] visited, int[][] dungeons, int curK, int count) {
+    //     maxCount = Math.max(count, maxCount);
+    //    for(int i = 0; i < dungeons.length; i++) {
+    //        if(!visited[i] && curK >= dungeons[i][0]) {
+    //            visited[i] = true;
+    //            visit(visited, dungeons, curK - dungeons[i][1], count + 1);
+    //            visited[i] = false;
+    //        }
+    //    }
+    // }
 //     public void visitDungeons(int[][] dungeons, boolean[] visited, int currentK, int count) {
 //         // 최대 방문 던전 수 갱신
 //         maxCount = Math.max(maxCount, count);
