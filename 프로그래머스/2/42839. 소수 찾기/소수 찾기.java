cc9484efe10a28db0d00a6ca986c37e37be57d72
acycoco <128036362+acycoco@users.noreply.github.com>
2025-02-18 20:48:44 +0900

    [level 2] Title: 소수 찾기, Time: 20.70 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -6,7 +6,7 @@ class Solution {
         char[] numbersC = numbers.toCharArray();
         used = new boolean[numbers.length()];
         
-        comb(0, numbersC.length, numbersC, "");
+        comb(numbersC, "");
         int count = 0;
         for(int n: answer) {
             if(isPrime(n)) {
@@ -16,25 +16,22 @@ class Solution {
         return count;
     }
     
-    public void comb(int depth, int n, char[] numbers, String now) {
-        if(depth == n) {
-            if( now.length() == 0) return;
+    public void comb(char[] numbers, String now) {
+        if(now.length() != 0) {
             answer.add(Integer.parseInt(now));
-            return;
         }
         
         for(int i = 0; i < numbers.length; i++) {
-            if(now.length() == 0 && numbers[i] == '0' || used[i]) continue; 
+            if(used[i]) continue; 
             used[i] = true;
-            comb(depth + 1, n, numbers, now  + numbers[i]);
+            comb(numbers, now  + numbers[i]);
             used[i] = false;
         }
-        comb(depth + 1, n, numbers, now);
     }
     
     
     public boolean isPrime(int n) {
-        if(n == 1) return false;
+        if(n < 2) return false;
         for(int i = 2; i <= Math.sqrt(n); i++) {
             if(n % i == 0) {
                 return false;
