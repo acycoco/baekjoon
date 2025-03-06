00ad66c7bf041331fb8bda577218a73094c31464
acycoco <128036362+acycoco@users.noreply.github.com>
2025-03-07 02:21:47 +0900

    [level 3] Title: 등굣길, Time: 1.16 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,28 +1,59 @@
 import java.util.*;
 class Solution {
-    int[] dx = {1, 0};
-    int[] dy = {0, 1};
-    int MOD = 1_000_000_007;
+    // int dx = {1, 0};
+    // int dy = {0, 1};
+    
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[m + 1][n + 1];
-      
-        for(int[] puddle: puddles) {
-            dp[puddle[0]][puddle[1]] = -1;
+        int[][] dp = new int[n + 1][m + 1];
+//         for(int i = 0; i <= n; i++) {
+//             dp[0][i] = 1; 
+//         }
+        
+//         for(int i = 0; i <= m; i++) {
+//             dp[i][0] = 1; 
+//         }
+        
+        for(int i = 0; i < puddles.length; i++) {
+            dp[puddles[i][1]][puddles[i][0]] = -1;
         }
+
         dp[1][1] = 1;
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
                 if(dp[i][j] == -1) continue;
-                if(dp[i - 1][j] != -1 && i > 1) {
-                    dp[i][j] += dp[i - 1][j];
-                    dp[i][j] %= MOD;
-                } 
-                if(dp[i][j - 1] != -1 && j > 1) {
-                    dp[i][j] += dp[i][j - 1];
-                    dp[i][j] %= MOD;
-                } 
+                dp[i][j] += (dp[i - 1][j] == -1 ? 0 : dp[i - 1][j]);
+                dp[i][j] += (dp[i][j - 1] == -1 ? 0 : dp[i][j - 1]);
+                dp[i][j] %= 1_000_000_007;
             }
         }
+
+        return dp[n][m];
+        
+        
+        
+        
+//         int[][] dp = new int[m + 1][n + 1];
+       
+//         for(int[] puddle: puddles) {
+//             dp[puddle[0]][puddle[1]] = -1;
+//         }
+//         dp[1][1] = 1;
+//         for(int i = 1; i <= m; i++) {
+//             for(int j = 1; j <= n; j++) {
+//                 if(dp[i][j] == -1) continue;
+//                 if(dp[i - 1][j] != -1 && i > 1) {
+//                     dp[i][j] += dp[i - 1][j];
+//                     dp[i][j] %= MOD;
+//                 } 
+//                 if(dp[i][j - 1] != -1 && j > 1) {
+//                     dp[i][j] += dp[i][j - 1];
+//                     dp[i][j] %= MOD;
+//                 } 
+//             }
+//         }
+        
+        
+        
 //         int answer = 0;
 //         Queue<Node> queue = new LinkedList<>();
 //         boolean[][] visited = new boolean[m + 1][n + 1];
@@ -58,7 +89,7 @@ class Solution {
 //         }
         
 //         System.out.println(minDistance);
-        return dp[m][n];
+        // return dp[m][n];
     }
     
 //     static class Node {
