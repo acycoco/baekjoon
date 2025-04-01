aa74de6f5a4e2950ff4983d7634b315bee359c24
acycoco <128036362+acycoco@users.noreply.github.com>
2025-04-01 20:19:29 +0900

    [Gold II] Title: 로봇 조종하기, Time: 532 ms, Memory: 90004 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -1,11 +1,13 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[][] arr, dp;
+    static int n;
+    static int m;
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +17,7 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n][m];
-        dp = new int[n][m];
+        int[][] dp = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -24,30 +26,26 @@ public class Main {
             }
         }
 
-        // 첫 번째 줄은 누적
+
         dp[0][0] = arr[0][0];
-        for (int j = 1; j < m; j++) {
-            dp[0][j] = dp[0][j - 1] + arr[0][j];
+        for (int i = 1; i < m; i++) {
+            dp[0][i] = dp[0][i - 1] + arr[0][i];
         }
 
-        // 두 번째 줄부터 처리
         for (int i = 1; i < n; i++) {
             int[] left = new int[m];
             int[] right = new int[m];
 
-            // 왼쪽에서 오른쪽으로 누적
             left[0] = dp[i - 1][0] + arr[i][0];
             for (int j = 1; j < m; j++) {
                 left[j] = Math.max(left[j - 1], dp[i - 1][j]) + arr[i][j];
             }
 
-            // 오른쪽에서 왼쪽으로 누적
             right[m - 1] = dp[i - 1][m - 1] + arr[i][m - 1];
             for (int j = m - 2; j >= 0; j--) {
                 right[j] = Math.max(right[j + 1], dp[i - 1][j]) + arr[i][j];
             }
 
-            // 둘 중 큰 값으로 dp 갱신
             for (int j = 0; j < m; j++) {
                 dp[i][j] = Math.max(left[j], right[j]);
             }
