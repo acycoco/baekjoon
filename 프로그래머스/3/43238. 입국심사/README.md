f1e21c6c76e7f952e4e9c69d6ff13d01a2b33979
acycoco <128036362+acycoco@users.noreply.github.com>
2025-04-03 21:59:46 +0900

    [level 3] Title: 입국심사, Time: 63.85 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?itm_content=course14743) 
 
 ### 성능 요약
 
-메모리: 96.8 MB, 시간: 44.37 ms
+메모리: 105 MB, 시간: 63.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 03:38:14
+2025년 04월 03일 21:59:44
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,35 +1,86 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        int minT = Integer.MAX_VALUE;
-        int maxT = Integer.MIN_VALUE;
+        long low = 1;
+        long high = Integer.MIN_VALUE;
         for(int i = 0; i < times.length; i++) {
-            minT = Math.min(times[i], minT);
-            maxT = Math.max(times[i], maxT);
+            high = Math.max(times[i], high);
         }
-        int minN;
-        int maxN;
-        maxN = n / times.length;
-        minN = maxN;
-        if(n % times.length != 0) {
-            minN = maxN + 1;
-        } 
-        long prevMax = maxT * maxN;//30  20  10
-        long prevMin = minT * minN;//21  28  35
-        long curMax = prevMax;
-        long curMin = prevMin;
-        while(Math.abs(prevMax - prevMin) >= Math.abs(curMax - curMin)) {
-            // System.out.println(prevMax +" "+ prevMin + " " + curMax + " "+curMin);
-            // maxN -= 1;
-            // minN += 1;
-            prevMax = curMax;
-            prevMin = curMin;
-            curMax -= maxT; //10
-            curMin += minT; //35
-            // System.out.println(prevMax +" "+ prevMin + " " + curMax + " "+curMin);
+        
+        high *= n;
+        long answer = 0;
+        while(low <= high) {
+            long mid = (low + high) / 2;
+            long total = 0;
+            for(int time: times) {
+                total += mid / time;
+                if (total >= n) {
+                    break;
+                }
+            }
+            
+            if(total >= n) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        long answer = Math.max(prevMax, prevMin);
         return answer;
+//         long low = 1;
+//         long high = Integer.MIN_VALUE;
+//         for(int i = 0; i < times.length; i++) {
+//             high = Math.max(times[i], high);
+//         }
+//         high *= n;
+//         long answer = 0;
+        
+//         while (low <= high) {
+//             long mid = (low + high ) / 2;
+//             long total = 0;
+//             for(int time: times) {
+//                 total += mid / time;
+//                 if(total >= n) break;
+//             }
+            
+//             if(total >= n) {
+//                 answer = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+           
+//         }
+//          return answer;
+        // int minT = Integer.MAX_VALUE;
+        // int maxT = Integer.MIN_VALUE;
+        // for(int i = 0; i < times.length; i++) {
+        //     minT = Math.min(times[i], minT);
+        //     maxT = Math.max(times[i], maxT);
+        // }
+        // int minN;
+        // int maxN;
+        // maxN = n / times.length;
+        // minN = maxN;
+        // if(n % times.length != 0) {
+        //     minN = maxN + 1;
+        // } 
+        // long prevMax = maxT * maxN;//30  20  10
+        // long prevMin = minT * minN;//21  28  35
+        // long curMax = prevMax;
+        // long curMin = prevMin;
+        // while(Math.abs(prevMax - prevMin) >= Math.abs(curMax - curMin)) {
+        //     // System.out.println(prevMax +" "+ prevMin + " " + curMax + " "+curMin);
+        //     // maxN -= 1;
+        //     // minN += 1;
+        //     prevMax = curMax;
+        //     prevMin = curMin;
+        //     curMax -= maxT; //10
+        //     curMin += minT; //35
+        //     // System.out.println(prevMax +" "+ prevMin + " " + curMax + " "+curMin);
+        // }
+        // long answer = Math.max(prevMax, prevMin);
+        // return answer;
     }
     //3  3     21  30      9    min값보다 차이가 클경우
     //4  2     28  20      -8    작아졌다가 커지는 게 정답(둘 중에는 최댓값)
