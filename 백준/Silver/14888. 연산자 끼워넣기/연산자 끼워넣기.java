d0f90f7e2d18bba9225fbbd1104d0a3f5255160e
acycoco <128036362+acycoco@users.noreply.github.com>
2025-04-19 02:15:04 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,55 +1,62 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
 
+public class Main {
     static int n;
-    static int[] arr;
-    static int[] operations = new int[4]; // + - x /
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
+    static int[] nums;
+    static int[] oper;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        nums = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
+         oper = new int[4];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operations[i] = Integer.parseInt(st.nextToken());
+            oper[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(1, arr[0]);
+        dfs(1, nums[0]);
         System.out.println(max);
         System.out.println(min);
+
     }
 
-    public static void dfs(int depth, int nowNumber) {
+    public static void dfs(int depth, int cur) {
         if (depth == n) {
-            max = Math.max(max, nowNumber);
-            min = Math.min(min, nowNumber);
+            max = Math.max(max, cur);
+            min = Math.min(min, cur);
+            return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (operations[i] > 0) {
-                operations[i]--;
-                dfs(depth + 1, compute(nowNumber, arr[depth], i));
-                operations[i]++;
+            if (oper[i] <= 0) {
+                continue;
             }
+            oper[i]--;
+            dfs(depth + 1, calc(cur, nums[depth], i));
+            oper[i]++;
         }
     }
-    public static int compute(int a, int b, int operation) {
-        switch (operation) {
-            case 0 : return a + b;
-            case 1 : return a - b;
-            case 2 : return a * b;
-            case 3 : return a / b;
-            default: return 0;
+
+    public static int calc(int a, int b, int operand) {
+        if (operand == 0) {
+            return a + b;
+        } else if (operand == 1) {
+            return a - b;
+        } else if (operand == 2) {
+            return a * b;
+        } else {
+            return a / b;
         }
     }
+
 }
